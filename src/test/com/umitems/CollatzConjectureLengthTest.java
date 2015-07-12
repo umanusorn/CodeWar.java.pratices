6bbcd25f89b87ef13e2cba66e696b8a3bafa6948
um2015 <umitems@gmail.com>
2015-07-12 15:41:51 +0700

    #6 Split the test case in Collatz

--- src/test/com/umitems/CollatzConjectureLengthTest.java
+++ src/test/com/umitems/CollatzConjectureLengthTest.java
@@ -13,55 +13,65 @@ import static org.junit.Assert.assertEquals;
 public class CollatzConjectureLengthTest {
   final String CLASS_NAME = new Object() {
   }.getClass().getName();
-
-  int input;
-  int output;
   @Rule
   public TestName name = new TestName();
+  int input;
+  int output;
+
   @Test
   public void testWith1() {
     String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
 
-    input =1;
-    output=3;
-    assertEquals(testName+ConstTest.GIVE+ input+ConstTest.SHOULD_B+output, output, CollatzConjectureLength.main(1));
-    input =2;
-    output=2;
-    assertEquals(testName+ConstTest.GIVE+ input+ConstTest.SHOULD_B+output, output, CollatzConjectureLength.main(2));
-    input =3;
-    output=8;
-    assertEquals(testName+ConstTest.GIVE+ input+ConstTest.SHOULD_B+output, output, CollatzConjectureLength.main(3));
-    input =4;
-    output=3;
-    assertEquals(testName+ConstTest.GIVE+ input+ConstTest.SHOULD_B+output, output, CollatzConjectureLength.main(4));
-    input =5;
-    output=6;
-    assertEquals(testName+ConstTest.GIVE+ input+ConstTest.SHOULD_B+output, output, CollatzConjectureLength.main(5));
+    input = 1;
+    output = 3;
+    assertEquals(testName + ConstTest.GIVE + input + ConstTest.SHOULD_B + output, output, CollatzConjectureLength.main(1));
   }
 
+  @Test
+  public void testWith2() {
 
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
+    input = 2;
+    output = 2;
+    assertEquals(testName + ConstTest.GIVE + input + ConstTest.SHOULD_B + output, output, CollatzConjectureLength.main(2));
+  }
+
+  @Test
+  public void testWith3() {
+
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
+    input = 3;
+    output = 8;
+    assertEquals(testName + ConstTest.GIVE + input + ConstTest.SHOULD_B + output, output, CollatzConjectureLength.main(3));
+  }
+
+  @Test
+  public void testWith4() {
+
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
+    input = 4;
+    output = 3;
+    assertEquals(testName + ConstTest.GIVE + input + ConstTest.SHOULD_B + output, output, CollatzConjectureLength.main(4));
+  }
+
+  @Test
+  public void testWith5() {
+
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
+    input = 5;
+    output = 6;
+    assertEquals(testName + ConstTest.GIVE + input + ConstTest.SHOULD_B + output, output, CollatzConjectureLength.main(5));
+  }
 
   @Test
   public void bigNumTest() {
-    String testName = CLASS_NAME + "/" +name.getMethodName() + "\n the input is";
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n the input is";
     long bigNumber = 0;
     long outputl;
 
-    bigNumber = 123456789l;
-    outputl =  conjecture(bigNumber);
-    assertEquals(testName+ConstTest.GIVE+bigNumber+ConstTest.SHOULD_B+outputl, outputl, CollatzConjectureLength.main(bigNumber));
-    bigNumber = 110101010101l;
-    outputl =  conjecture(bigNumber);
-    assertEquals(testName+ConstTest.GIVE+bigNumber+ConstTest.SHOULD_B+outputl, outputl, CollatzConjectureLength.main(bigNumber));
     bigNumber = 987654321123456789l;
-    outputl =  conjecture(bigNumber);
-    assertEquals(testName+ConstTest.GIVE+bigNumber+ConstTest.SHOULD_B+outputl, outputl, CollatzConjectureLength.main(bigNumber));
-    bigNumber = 1000000000000000000l;
-    outputl =  conjecture(bigNumber);
-    assertEquals(testName+ConstTest.GIVE+bigNumber+ConstTest.SHOULD_B+outputl, outputl, CollatzConjectureLength.main(bigNumber));
-    bigNumber = 1111111111111111111l;
-    outputl =  conjecture(bigNumber);
-    assertEquals(testName+ConstTest.GIVE+bigNumber+ConstTest.SHOULD_B+outputl, outputl, CollatzConjectureLength.main(bigNumber));
+    outputl = conjecture(bigNumber);
+    assertEquals(testName + ConstTest.GIVE + bigNumber + ConstTest.SHOULD_B + outputl, outputl, CollatzConjectureLength.main(bigNumber));
 
     /*bigNumber = Long.MAX_VALUE; error i think it's because of solution's code :P
     outputl =  conjecture(bigNumber);
@@ -70,14 +80,14 @@ public class CollatzConjectureLengthTest {
   }
 
   @Test
-  public void randomTest(){
-    String testName = CLASS_NAME + "/" + name.getMethodName()+ "\n";
+  public void randomTest() {
+    String testName = CLASS_NAME + "/" + name.getMethodName() + "\n";
     final long INIT_17DIGIT = 100000000000000000L;
 
-    for(int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++) {
       long rand17Digit = (long) Math.floor(Math.random() * (INIT_17DIGIT));
       long outputl = conjecture(rand17Digit);
-      assertEquals(testName+ConstTest.GIVE+rand17Digit+ConstTest.SHOULD_B+outputl,outputl, CollatzConjectureLength.main(rand17Digit)) ;
+      assertEquals(testName + ConstTest.GIVE + rand17Digit + ConstTest.SHOULD_B + outputl, outputl, CollatzConjectureLength.main(rand17Digit));
     }
 
     //the solution test case goes with long rand = (long) Math.floor(Math.random() * (1000000000000000L - 1000000L) + 1000000L);
